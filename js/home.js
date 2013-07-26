1b1b252bf7a7ac059fc229944bcd24ef213822f1
huugle <511194948@qq.com>
2013-07-26 08:36:18 +0800

    新增列表页
    新增列表页,修改一些兼容问题

--- js/home.js
+++ js/home.js
@@ -1,12 +1,47 @@
-
 $(function() {
 	//keyword
-var keyword = $("#keyword");
-keyword.bind("focus", function() {
+	var keyword = $("#keyword");
+	keyword.bind("focus", function() {
 		$(".keyword").fadeIn();
 	});
-keyword.bind("blur",function() {
+	keyword.bind("blur", function() {
 		$(".keyword").fadeOut();
 	});
+	//up &down
+	jQuery.easing['easeInOutElastic'] = function(x, t, b, c, d) {
+		return -c * ((t = t / d - 1) * t * t * t - 1) + b;
+	};
+	$(".i-more").bind("click", function() {
+		var eve = $(this).parents(".m-b").find(".m-cent");
+		var $this = $(this);
+		eve.slideToggle(350, "easeInOutElastic", function() {
+			$this.toggleClass("i-more-up");
+		});
+	});
+	//op
+	$(".i-open").bind("click", function() {
+		var $this = $(this);
+		var right = $this.css("right");
+		var m = (right == "-27px") ? -167 : -27;
+		$this.animate({
+			"right": m
+		}, 250, function() {
+			$this.toggleClass("i-close");
+		});
+		$(".u-d").fadeToggle(350);
+	});
+	//heck 
+	var isIE = document.all && !document.querySelector;
+	if (isIE) {
+		$(".ms-list li").hover(function() {
+			$(this).css({
+				"z-index": 100
+			});
+		}, function() {
+			$(this).css({
+				"z-index": 1
+			});
+		})
+	}
 
 })
